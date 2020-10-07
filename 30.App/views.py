99812a42208b7299833294118549472813f273c1
unknown <ntduc2812@gmail.com>
2020-10-07 22:13:29 +0700

    Xu ly permission cho ctv

--- 30.App/views.py
+++ 30.App/views.py
@@ -221,6 +221,11 @@ def login_admin_process():
       return redirect("/login-admin")
    return redirect("/admin")
 
+@app.route('/error')
+@login_required
+def error():
+   return render_template('admin/404.html')  
+
 @app.route('/admin')
 @login_required
 def admin():
@@ -284,138 +289,63 @@ def admin():
             sold.append(nick)
 
          # Convert nicks data
-         user = session.query(models.Users).filter(models.Users.id == nick.user_id).first()
          data = {
             "id": nick.id,
             "code": nick.code,
             "name": nick.name,
             "game_name": nick.game_name,
             "price": nick.price,
-            "user": { "name": user.name, "id": user.id },
             "status": nick.status,
             "game_info": json.loads(nick.game_info)
          }
          nicks.append(data)
       return render_template(
-         'admin/admin.html',
+         'admin/ctv-nick.html',
          nicks=nicks,
          sold=sold,
          on_sale=on_sale)
    else:
-      return redirect("/")
+      return redirect("/404")
 
 @app.route('/add-nick', methods=['POST'])
 def add_nick():
-   # try:
-   name = request.form.get('name')
-   price = request.form.get('price')
-   game_type = request.form.get('gameType')
+   try:
+      name = request.form.get('name')
+      price = request.form.get('price')
+      game_type = request.form.get('gameType')
 
-   code = ''
-   # Game Info
-   if game_type=='LQ':
-      code  = 'LQ-' + str(int(datetime.datetime.utcnow().timestamp()))
-      game_name = 'Liên Quân'
-      data = {
-         "account": request.form.get('account'),
-         "password": request.form.get('password'),
-         "rank": request.form.get('rank'),
-         "tuong": request.form.get('slTuong'),
-         "skin": request.form.get('Skin'),
-         "ngoc": request.form.get('ngoc'),
-         "da_quy": request.form.get('daQuy')
-      }
-   else:
-      code  = 'NR-' + str(int(datetime.datetime.utcnow().timestamp()))
-      game_name = 'Ngọc Rồng'
-      data = {
-         "nickType": request.form.get('nickType'),
-         "server": request.form.get('server'),
-         "hanhTinh": request.form.get('hanhTinh'),
-         "bongTai": True if request.form.get('bongTai') == 'on' else False,
-         "deTu": True if request.form.get('deTu') == 'on' else False
-      }
+      code = ''
+      # Game Info
+      if game_type=='LQ':
+         code  = 'LQ-' + str(int(datetime.datetime.utcnow().timestamp()))
+         game_name = 'Liên Quân'
+         data = {
+            "account": request.form.get('account'),
+            "password": request.form.get('password'),
+            "rank": request.form.get('rank'),
+            "tuong": request.form.get('slTuong'),
+            "skin": request.form.get('Skin'),
+            "ngoc": request.form.get('ngoc'),
+            "da_quy": request.form.get('daQuy')
+         }
+      else:
+         code  = 'NR-' + str(int(datetime.datetime.utcnow().timestamp()))
+         game_name = 'Ngọc Rồng'
+         data = {
+            "nickType": request.form.get('nickType'),
+            "server": request.form.get('server'),
+            "hanhTinh": request.form.get('hanhTinh'),
+            "bongTai": True if request.form.get('bongTai') == 'on' else False,
+            "deTu": True if request.form.get('deTu') == 'on' else False
+         }
 
-   path = app.config['UPLOAD_FOLDER'] + code
-   os.mkdir(path)
+      path = app.config['UPLOAD_FOLDER'] + code
+      os.mkdir(path)
 
-   # Image
-   if 'files' not in request.files:
-      flash('No file part')
-      return redirect("/admin")
-   images = request.files.getlist('files')
-   # if user does not select file, browser also
-   # submit an empty part without filename
-   list_image = []
-   if images:
-      for image in images:
-         if image.filename == '':
-            flash('No selected file')
-            return redirect("/admin")
-         file_name = common.upload_file(image, path)
-         list_image.append(os.path.join(UPLOAD_FOLDER + code, image.filename))
-
-   nick = models.Nicks()
-   nick.name = name
-   nick.price = price
-   nick.status = 'Xác nhận'
-   nick.game_name = game_name
-   nick.game_info = json.dumps(data)
-   nick.code = code
-   nick.create_at = str(datetime.datetime.now())
-   nick.images = ','.join(list_image)
-   nick.user_id = current_user.id
-   session.add(nick)
-   session.commit()
-   session.close()
-
-   flash('Đăng ký bán nick thành công!')
-   # except Exception as e:
-   #    flash(e)
-   #    flash('Hệ thống lỗi, nhờ báo cáo sự cố với bộ phận kỹ thuật.')
-   #    redirect("/admin")
-   return redirect("/admin")
-
-@app.route('/edit-nick', methods=['POST'])
-def edit_nick():
-   # try:
-   id = request.form.get('id')
-   nick = session.query(models.Nicks).filter(models.Nicks.id == id).first()
-   name = request.form.get('name')
-   nick.name = name
-   price = request.form.get('price')
-   nick.price = price
-   game_type = request.form.get('gameType')
-
-   code = ''
-   # Game Info
-   if game_type=='LQ':
-      code  = 'LQ-' + str(int(datetime.datetime.utcnow().timestamp()))
-      game_name = 'Liên Quân'
-      data = {
-         "account": request.form.get('account'),
-         "password": request.form.get('password'),
-         "rank": request.form.get('rank'),
-         "tuong": request.form.get('slTuong'),
-         "skin": request.form.get('Skin'),
-         "ngoc": request.form.get('ngoc'),
-         "da_quy": request.form.get('daQuy')
-      }
-   else:
-      code  = 'NR-' + str(int(datetime.datetime.utcnow().timestamp()))
-      game_name = 'Ngọc Rồng'
-      data = {
-         "nickType": request.form.get('nickType'),
-         "server": request.form.get('server'),
-         "hanhTinh": request.form.get('hanhTinh'),
-         "bongTai": True if request.form.get('bongTai') == 'on' else False,
-         "deTu": True if request.form.get('deTu') == 'on' else False
-      }
-
-   # Image
-   if 'files' not in request.files:
-      nick.images = nick.images
-   else:
+      # Image
+      if 'files' not in request.files:
+         flash('No file part')
+         return redirect("/admin")
       images = request.files.getlist('files')
       # if user does not select file, browser also
       # submit an empty part without filename
@@ -423,29 +353,102 @@ def edit_nick():
       if images:
          for image in images:
             if image.filename == '':
-               nick.images = nick.images
-            else:
-               path = app.config['UPLOAD_FOLDER'] + code
-               os.mkdir(path)
-               file_name = common.upload_file(image, path)
-               list_image.append(os.path.join(UPLOAD_FOLDER + code, image.filename))
-               nick.images = ','.join(list_image)
+               flash('No selected file')
+               return redirect("/admin")
+            file_name = common.upload_file(image, path)
+            list_image.append(os.path.join(UPLOAD_FOLDER + code, image.filename))
 
-   nick.status = request.form.get('status')
-   nick.game_name = game_name
-   nick.game_info = json.dumps(data)
-   nick.code = code
-   nick.create_at = str(datetime.datetime.now())
-   nick.user_id = current_user.id
-   session.merge(nick)
-   session.commit()
-   session.close()
+      nick = models.Nicks()
+      nick.name = name
+      nick.price = price
+      nick.status = 'Xác nhận'
+      nick.game_name = game_name
+      nick.game_info = json.dumps(data)
+      nick.code = code
+      nick.create_at = str(datetime.datetime.now())
+      nick.images = ','.join(list_image)
+      nick.user_id = current_user.id
+      session.add(nick)
+      session.commit()
+      session.close()
 
-   flash('Update thông tin nick thành công!')
-   # except Exception as e:
-   #    flash(e)
-   #    flash('Hệ thống lỗi, nhờ báo cáo sự cố với bộ phận kỹ thuật.')
-   #    redirect("/admin")
+      flash('Đăng ký bán nick '+ code + ' thành công!')
+   except Exception as e:
+      flash(e)
+      flash('Hệ thống lỗi, nhờ báo cáo sự cố với bộ phận kỹ thuật.')
+      redirect("/admin")
+   return redirect("/admin")
+
+@app.route('/edit-nick', methods=['POST'])
+def edit_nick():
+   try:
+      id = request.form.get('id')
+      nick = session.query(models.Nicks).filter(models.Nicks.id == id).first()
+      name = request.form.get('name')
+      nick.name = name
+      price = request.form.get('price')
+      nick.price = price
+      game_type = request.form.get('gameType')
+
+      code = ''
+      # Game Info
+      if game_type=='LQ':
+         code  = 'LQ-' + str(int(datetime.datetime.utcnow().timestamp()))
+         game_name = 'Liên Quân'
+         data = {
+            "account": request.form.get('account'),
+            "password": request.form.get('password'),
+            "rank": request.form.get('rank'),
+            "tuong": request.form.get('slTuong'),
+            "skin": request.form.get('Skin'),
+            "ngoc": request.form.get('ngoc'),
+            "da_quy": request.form.get('daQuy')
+         }
+      else:
+         code  = 'NR-' + str(int(datetime.datetime.utcnow().timestamp()))
+         game_name = 'Ngọc Rồng'
+         data = {
+            "nickType": request.form.get('nickType'),
+            "server": request.form.get('server'),
+            "hanhTinh": request.form.get('hanhTinh'),
+            "bongTai": True if request.form.get('bongTai') == 'on' else False,
+            "deTu": True if request.form.get('deTu') == 'on' else False
+         }
+
+      # Image
+      if 'files' not in request.files:
+         nick.images = nick.images
+      else:
+         images = request.files.getlist('files')
+         # if user does not select file, browser also
+         # submit an empty part without filename
+         list_image = []
+         if images:
+            for image in images:
+               if image.filename == '':
+                  nick.images = nick.images
+               else:
+                  path = app.config['UPLOAD_FOLDER'] + code
+                  os.mkdir(path)
+                  file_name = common.upload_file(image, path)
+                  list_image.append(os.path.join(UPLOAD_FOLDER + code, image.filename))
+                  nick.images = ','.join(list_image)
+
+      nick.status = request.form.get('status')
+      nick.game_name = game_name
+      nick.game_info = json.dumps(data)
+      nick.code = code
+      nick.create_at = str(datetime.datetime.now())
+      nick.user_id = current_user.id
+      session.merge(nick)
+      session.commit()
+      session.close()
+
+      flash('Update thông tin nick thành công!')
+   except Exception as e:
+      flash(e)
+      flash('Hệ thống lỗi, nhờ báo cáo sự cố với bộ phận kỹ thuật.')
+      redirect("/admin")
    return redirect("/admin")
 
 @app.route('/delete-nick-<id>')
@@ -460,41 +463,47 @@ def delete_nick(id):
 
 @app.route('/admin-ctv')
 def admin_ctv():
-   # Get info nicks
-   nicks_qr = session.query(models.Nicks).all()
-   sold = []
-   on_sale = []
-   nicks = []
-   for nick in nicks_qr:
-      if nick.status == 'Đang bán':
-         on_sale.append(nick)
-      if nick.status == 'Đã bán':
-         sold.append(nick)
+   per = checkPermisson()
+   if per == 'admin':
+      # Get info nicks
+      nicks_qr = session.query(models.Nicks).all()
+      sold = []
+      on_sale = []
+      nicks = []
+      for nick in nicks_qr:
+         if nick.status == 'Đang bán':
+            on_sale.append(nick)
+         if nick.status == 'Đã bán':
+            sold.append(nick)
 
-      # Convert nicks data
-      user = session.query(models.Users).filter(models.Users.id == nick.user_id).first()
-      data = {
-         "id": nick.id,
-         "code": nick.code,
-         "name": nick.name,
-         "game_name": nick.game_name,
-         "price": nick.price,
-         "user": { "name": user.name, "id": user.id },
-         "status": nick.status
-      }
-      nicks.append(data)
+         # Convert nicks data
+         user = session.query(models.Users).filter(models.Users.id == nick.user_id).first()
+         historys = session.query(models.History).filter(models.History.user_id == nick.user_id).all()
+         data = {
+            "id": nick.id,
+            "code": nick.code,
+            "name": nick.name,
+            "sale": len(historys),
+            "game_name": nick.game_name,
+            "price": nick.price,
+            "user": { "name": user.name, "id": user.id },
+            "status": nick.status
+         }
+         nicks.append(data)
 
-   # Get info users
-   ctv = []
-   enduser = []
-   users = []
-   users_qr = session.query(models.Users).filter(models.Users.super == False).all()
-   for user in users_qr:
-      if user.ctv:
-         ctv.append(user)
-      elif user.enduser:
-         enduser.append(user)
-   return render_template('admin/ctv.html', nicks=nicks, sold=sold, on_sale=on_sale, ctv=ctv, enduser=enduser)
+      # Get info users
+      ctv = []
+      enduser = []
+      users = []
+      users_qr = session.query(models.Users).filter(models.Users.super == False).all()
+      for user in users_qr:
+         if user.ctv:
+            ctv.append(user)
+         elif user.enduser:
+            enduser.append(user)
+      return render_template('admin/ctv.html', nicks=nicks, sold=sold, on_sale=on_sale, ctv=ctv, enduser=enduser)
+   else:
+      return redirect("/error")
 
 @app.route('/add-ctv', methods=['POST'])
 def add_ctv():
@@ -516,49 +525,142 @@ def add_ctv():
       session.commit()
       session.close()
 
-      flash('Tạo ctv thành công!')
+      flash('Tạo ctv '+ account_tk +' thành công!')
    except Exception as e:
       flash(e)
       flash('Hệ thống lỗi, nhờ báo cáo sự cố với bộ phận kỹ thuật.')
    return redirect("/admin-ctv")
 
+@app.route('/edit-user', methods=['POST'])
+def edit_user():
+   per = checkPermisson()
+   if per == 'admin':
+      try:
+         id = request.form.get('id')
+         name = request.form.get('name')
+         account_tk = request.form.get('account_tk')
+         password = request.form.get('password')
+         money = request.form.get('money')
+         
+         user = session.query(models.Users).filter(models.Users.id == int(id)).first()
+         user.name = name
+         user.account_tk = account_tk
+         user.password = password
+         user.money = money
+         user.create_at = str(datetime.datetime.now())
+
+         session.merge(user)
+         session.commit()
+         session.close()
+
+         flash('Update user '+ account_tk +' thành công!')
+      except Exception as e:
+         flash(e)
+         flash('Hệ thống lỗi, nhờ báo cáo sự cố với bộ phận kỹ thuật.')
+      return redirect("/admin-ctv")
+   else:
+      return redirect("/error")
+
 @app.route('/admin-history')
 def admin_history():
-   # Get info nicks
-   nicks_qr = session.query(models.Nicks).all()
-   sold = []
-   on_sale = []
-   nicks = []
-   for nick in nicks_qr:
-      if nick.status == 'Đang bán':
-         on_sale.append(nick)
-      if nick.status == 'Đã bán':
-         sold.append(nick)
+   per = checkPermisson()
+   if per == 'admin':
+      # Get info nicks
+      nicks_qr = session.query(models.Nicks).all()
+      sold = []
+      on_sale = []
+      nicks = []
+      for nick in nicks_qr:
+         if nick.status == 'Đang bán':
+            on_sale.append(nick)
+         if nick.status == 'Đã bán':
+            sold.append(nick)
 
-      # Convert nicks data
-      user = session.query(models.Users).filter(models.Users.id == nick.user_id).first()
-      data = {
-         "id": nick.id,
-         "code": nick.code,
-         "name": nick.name,
-         "game_name": nick.game_name,
-         "price": nick.price,
-         "user": { "name": user.name, "id": user.id },
-         "status": nick.status
-      }
-      nicks.append(data)
+         # Convert nicks data
+         user = session.query(models.Users).filter(models.Users.id == nick.user_id).first()
+         data = {
+            "id": nick.id,
+            "code": nick.code,
+            "name": nick.name,
+            "game_name": nick.game_name,
+            "price": nick.price,
+            "user": { "name": user.name, "id": user.id },
+            "status": nick.status
+         }
+         nicks.append(data)
 
-   # Get info users
-   ctv = []
-   enduser = []
-   users = []
-   users_qr = session.query(models.Users).filter(models.Users.super == False).all()
-   for user in users_qr:
-      if user.ctv:
-         ctv.append(user)
-      elif user.enduser:
-         enduser.append(user)
-   return render_template('admin/ctv.html', nicks=nicks, sold=sold, on_sale=on_sale, ctv=ctv, enduser=enduser)
+      # Get info users
+      ctv = []
+      enduser = []
+      users = []
+      users_qr = session.query(models.Users).filter(models.Users.super == False).all()
+      for user in users_qr:
+         if user.ctv:
+            ctv.append(user)
+         elif user.enduser:
+            enduser.append(user)
+      
+      historys = session.query(models.History).all()
+      data = []
+      for his in historys:
+         # Convert nicks data
+         user = session.query(models.Users).filter(models.Users.id == his.user_id).first()
+         history = {
+            "id": his.id,
+            "card": his.card,
+            "buy": his.buy,
+            "info": json.loads(his.info),
+            "create_at": his.create_at,
+            "user": { "name": user.name, "id": user.id },
+            "status": his.status
+         }
+         data.append(history)
+
+      return render_template('admin/history.html', data=data, nicks=nicks, sold=sold, on_sale=on_sale, ctv=ctv, enduser=enduser)
+   else:
+      return redirect("/error")
+
+@app.route('/admin-user')
+def admin_user():
+   per = checkPermisson()
+   if per == 'admin':
+      # Get info nicks
+      nicks_qr = session.query(models.Nicks).all()
+      sold = []
+      on_sale = []
+      nicks = []
+      for nick in nicks_qr:
+         if nick.status == 'Đang bán':
+            on_sale.append(nick)
+         if nick.status == 'Đã bán':
+            sold.append(nick)
+
+         # Convert nicks data
+         user = session.query(models.Users).filter(models.Users.id == nick.user_id).first()
+         data = {
+            "id": nick.id,
+            "code": nick.code,
+            "name": nick.name,
+            "game_name": nick.game_name,
+            "price": nick.price,
+            "user": { "name": user.name, "id": user.id },
+            "status": nick.status
+         }
+         nicks.append(data)
+
+      # Get info users
+      ctv = []
+      enduser = []
+      users = []
+      users_qr = session.query(models.Users).filter(models.Users.super == False).all()
+      for user in users_qr:
+         if user.ctv:
+            ctv.append(user)
+         elif user.enduser:
+            enduser.append(user)
+      return render_template('admin/user.html', nicks=nicks, sold=sold, on_sale=on_sale, ctv=ctv, enduser=enduser)
+   else:
+      return redirect("/error")
 
 if __name__ == '__main__':
    app.run(debug = True)
